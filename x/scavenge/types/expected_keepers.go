96343aeadda0a9dca9a79cae1af145b7f5a00b27
Starlord0alpha <notgoatmichael@gmail.com>
2022-07-30 00:02:48 +0800

    add

--- x/scavenge/types/expected_keepers.go
+++ x/scavenge/types/expected_keepers.go
@@ -13,6 +13,5 @@ type AccountKeeper interface {
 
 // BankKeeper defines the expected interface needed to retrieve account balances.
 type BankKeeper interface {
-	SpendableCoins(ctx sdk.Context, addr sdk.AccAddress) sdk.Coins
-	// Methods imported from bank should be defined here
+	SendCoins(ctx sdk.Context, fromAddr sdk.AccAddress, toAddr sdk.AccAddress, amt sdk.Coins) error
 }
